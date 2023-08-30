c0da5f82dd641569acd2ceec414696c859329317
vLetha <yjhb1999@gmail.com>
2023-08-30 09:15:31 -0400

    fix CORS

--- index.js
+++ index.js
@@ -58,6 +58,15 @@ let proveedores = {
 	},
 };
 
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
+
 app.get('/json', (req, res) => {
 	res.json({ 'Choo Choo': 'Welcome to your Express app 🚅' });
 });
